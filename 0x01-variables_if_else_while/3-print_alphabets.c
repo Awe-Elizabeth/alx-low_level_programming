913ec408a284444c52e467e6695f4819246ef65f
Awe Titilayo <alizzyshire@gmail.com>
2022-09-09 21:38:21 +0000

    New

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,15 @@
  */
 int main(void)
 {
-	char c = "abcdefghijklmnopqrstuvwxyz";
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+{
 	putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+}
 	return (0);
 }
