89ccac9583000e9cb36423de2fa16f052ac48824
Awe Titilayo <alizzyshire@gmail.com>
2022-09-09 21:16:24 +0000

    Bet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Returns zero
+ */
+int main(void)
+{
+	char c = 'a';
+for (c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Returns zero
+ */
+int main(void)
+{
+	char c = "abcdefghijklmnopqrstuvwxyz";
+	putchar(c);
+	return (0);
+}
