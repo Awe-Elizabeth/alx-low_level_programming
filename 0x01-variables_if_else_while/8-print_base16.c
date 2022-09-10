5be28a8ab77ed87584a647d2972a44f1a694ee92
Awe Titilayo <alizzyshire@gmail.com>
2022-09-10 19:12:04 +0000

    Now

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,11 +5,16 @@
  */
 int main(void)
 {
-	int n;
-for (n = 'n'; n <= '9'; n++)
-{
-	putchar(n);
-}
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints single-digits numbers
+ * Return: returns zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
