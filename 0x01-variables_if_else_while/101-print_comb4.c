539b4ac706db2a0cb7469862089c7e90075af855
Awe Titilayo <alizzyshire@gmail.com>
2022-09-12 20:09:15 +0000

    Changes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the combo of three nums
+ * Return: returns zero
+ */
+int main(void)
+{
+	int i, j, k;
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 1; i <= 9; i++)
+		{
+			for (i = 2; i <= 9; i++)
+			{
+				if (i > j && i > k)
+
+			}
+		}
+	}
+}
