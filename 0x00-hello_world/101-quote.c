178f8545171f78db8fe3b3122009e4c0a70c57d7
Awe Titilayo <alizzyshire@gmail.com>
2022-09-12 20:08:45 +0000

    Changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: Entry
+ * Return: returns zero
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dor Korpar, 2015-10-19\n", 59);
+	return (1);
+}
