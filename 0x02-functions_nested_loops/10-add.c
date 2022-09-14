77303e138221b4eb1c938c717b8556feae6a5ba1
Awe Titilayo <alizzyshire@gmail.com>
2022-09-14 14:12:15 +0000

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - prints the result after adding two integers
+ * @one: first number
+ * @two: second number
+ * Return: returns addition
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,18 @@
 #include "main.h"
 /**
  * print_last_digit - prints last digits
- * @nld:
+ * @nld: last digit
+ * Return: returns the last digit
+ */
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10);
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+	_putchar(pld + '0');
+	return (pld);
+}
