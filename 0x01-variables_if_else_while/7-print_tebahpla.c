83ec4f4f62e2e0646b547a728a138153fe78b8f4
Awe Elizabeth <alizzyshire@gmail.com>
2022-09-13 21:12:48 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
-* main- Entry
-* Return: returns zero
+* print_last_digit - a function that prints the last digit of a numb
+* @nld: number's last digit result
+* Return: value of the last digit
 */
+
 int main(void)
 {
 	char c;
