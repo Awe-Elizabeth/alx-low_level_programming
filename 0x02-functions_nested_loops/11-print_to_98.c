d8a4fbcedf8e9e8a351f86784ac86f639c2a83ff
Awe Titilayo <alizzyshire@gmail.com>
2022-09-14 14:28:14 +0000

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include "main"
 /**
- * prnt_to_98 - prints rational numbers to 28
- *@n: parameter
-Return: Return 
-/
-int print-to_98(int n)
+ * print_to_98 - prints rational numbers to 28
+ * @n: parameter
+ *Return: Return
+ */
+int print_to_98(int n)
 {
 	if (n < 98)
 	{
 		while (n <= 98)
-		{}
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++
+		}
 	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	print("\n");
 }
