c809c4aff0f16181e140e41fd815955464cb52e9
Awe Elizabeth <alizzyshire@gmail.com>
2022-09-13 22:05:16 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,31 @@ int print-to_98(int n)
 	if (n < 98)
 	{
 		while (n <= 98)
-		{}
+		{
+			printf("%d", n);
+			if (n !98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
 	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+			printf(", ");
+			}
+			n--
+
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
