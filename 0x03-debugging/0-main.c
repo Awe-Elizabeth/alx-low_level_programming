0470a9d7f2b41fa5fffe53a721b7d276960db769
Awe Elizabeth <alizzyshire@gmail.com>
2022-09-15 18:22:26 -0700

    Tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
   * main - Test function for positive or negative
@@ -14,17 +13,4 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
-=======
-/**
- * main - tests function that prints int
- * Return: 0
- */
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-	return (0);
->>>>>>> 1b81c9f097ae9a7a6f9f9ff60f5affcaf4ca9fde
 }
