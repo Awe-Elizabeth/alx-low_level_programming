b7ec75047e1e83a57964e6a7129a04e2daaf0c29
Awe Titilayo <alizzyshire@gmail.com>
2022-10-13 18:31:53 +0000

    It

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,12 +5,13 @@
  * @array: the array argument
  * @size: the array size
  * @action: function pointer
+ * return: returns void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array != NULL && size > 0 && action != NULL)
 	{
-		for (int i = 0; i < size; i++)
+		for (size_t i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
