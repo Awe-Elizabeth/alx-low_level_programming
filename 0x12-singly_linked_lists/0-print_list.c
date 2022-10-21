9340c322b086d9492d13913719a100069ce2c05f
Awe Titilayo <alizzyshire@gmail.com>
2022-10-21 00:51:27 +0000

    List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,16 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)");
 	while (h != NULL)
 	{
-		i = i + 1;
-		h->next;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		} else
+
+		printf("[%lu] %s\n", h->len, h->str);
+		h = h->next;
+		i++
 	}
 	return (i);
 }
