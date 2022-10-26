cf4f20b9215e35c0d06045dc3c2947011b99bdf8
Awe Titilayo <alizzyshire@gmail.com>
2022-10-26 00:18:10 +0000

    linked

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - The function that prints all elements
+ * @h: the head element
+ * Return: returns type of size_t
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++
+		h = h->next;
+	}
+	return (count);
+}
