8b1382ccf3c43ac053594dae0d37574b0625198f
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-12 13:43:18 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/m_func.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _isdigit - function thwt checks if an argument is digit or not
+ * @c: The parameter
+ * Return: returns 0 or 1
+ */
+int _isdigit(int c)
+{
+
+}
+
+/**
+ * _strlen - function the gives tye length of a string
+ * @s: parameter
+ * Return: void
+ */
+int _strlen(char *s)
+{
+
+}
+
+/**
+ * _puts - a function that outs
+ * @s: the s parameter
+ */
+void _puts(char *s)
+{
+
+}
+
+/**
+ * _strcpy - the functios that gives a copy
+ * @dest: The dest
+ * @src: character
+ * Return: returns a character
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+}
+
+/**
+ * _atoi - changes char to int
+ * @s: character s
+ * Return: returns an int
+ */
+int _atoi(char *s)
+{
+
+}
---
+++ 0x18-dynamic_libraries/s_func.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _putchar - function that wrute the putchar
+ * @c: parameter c
+ * Return: returns 0 or 1
+ */
+int _putchar(char c)
+{
+	
+}
+
+/**
+ * _islower - function that prints lower alpha
+ * @c: charcter parameter
+ * Return: returns an int
+ */
+int _islower(char c)
+{
+	
+}
+
+/**
+ * _isalpha - chacks if a characyer is an alpha
+ * @c: yhe parameter c
+ * Return: returns 1 or 0
+ */
+int _isalpha(char c)
+{
+	
+}
+
+/**
+ * _abs - o don't comp
+ * @n: pareter n
+ * Return: returns 1 or 0
+ */
+int _abs(int n)
+{
+
+}
+
+/**
+ * _isupper - Ch3cks of c is upper
+ * @c: the parameter
+ * Retuen: returns 1 or 0
+ */
+int _isupper(int c)
+{
+
+}
