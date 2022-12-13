e8f9a392572ce3474852aa65f5a909f033e2e500
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-13 03:00:48 -0800

    Git

--- 0x18-dynamic_libraries/u_func.c
+++ 0x18-dynamic_libraries/u_func.c
@@ -21,3 +21,38 @@ char *_strstr(char *haystack, char *needle)
 {
 
 }
+
+/**
+ * _strcat - function
+ * @dest: parameter
+ * @src: parameter
+ * Return: returns char
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
+
+/**
+ * _strncat - function
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: returns char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
+
+/**
+ * _strncpy - function
+ * @dest: parameyer
+ * @src: parameter
+ * @n: parameter
+ * Return: returns an int
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
