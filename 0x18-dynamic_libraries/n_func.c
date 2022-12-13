0a99ec0f92f8dbe102788d3ba9181d9cfcc5e93d
Awe Elizabeth <alizzyshire@gmail.com>
2022-12-13 02:28:37 -0800

    New

---
+++ 0x18-dynamic_libraries/n_func.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strcmp - a function
+ * @s1: parameter
+ * @s2: second parameter
+ * Return: returns 0 or 1
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+}
+
+/**
+ * _memset - a function
+ * @s: yhe parameter
+ * @b: another parameter
+ * @n: integer
+ * Return: returns a char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
+
+/**
+ * _memcpy - a fumction
+ * @dest: parameter
+ * @src: a parameter
+ * @n: a parameter
+ * Return: returns character
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
+/**
+ * _strchr - a function
+ * @s: parameter
+ * @c: parameter
+ * Return: returns a character
+ */
+char *_strchr(char *s, char c)
+{
+
+}
+
+/**
+ * _strspn - a functiob
+ * @s: parameter
+ * @accept: parameter
+ * Return: returns an int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
---
+++ 0x18-dynamic_libraries/u_func.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strpbrk - function
+ * @s: parameter
+ * @accept: parameter
+ * Return: returns a character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+}
+/**
+ * _strstr - a function
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: returns a char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
